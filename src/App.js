b479d099a81113d525081cad92891d52da56b622
anggasayogo <anggasayogosm@gmail.com>
2020-09-08 19:29:24 +0700

    inital app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mx-auto bg-orange-100">
+      <h1 className="text-6xl">Hello My app</h1>
+    </div>
+  );
+}
+
+export default App;
